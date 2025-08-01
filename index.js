142ba860b9e0f1ca91969d7e642ee66b8a61384d
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2025-08-01 16:51:22 +0900

    Bubbles lose air over time

--- index.js
+++ index.js
@@ -16,6 +16,30 @@ const port = process.env.PORT || 8080;
 // Track click count
 let clickCount = 0;
 
+// Air loss timer - bubble loses 1 click every 5 seconds
+const AIR_LOSS_INTERVAL = 5000; // 5 seconds
+let airLossTimer = null;
+
+const startAirLoss = () => {
+  if (airLossTimer) clearInterval(airLossTimer);
+
+  airLossTimer = setInterval(() => {
+    if (clickCount > 0) {
+      clickCount--;
+      console.log(`Air loss! Bubble shrunk to: ${clickCount}`);
+
+      // Broadcast air loss to all clients
+      io.emit('clickUpdate', {
+        count: clickCount,
+        reason: 'air_loss'
+      });
+    }
+  }, AIR_LOSS_INTERVAL);
+};
+
+// Start air loss when server starts
+startAirLoss();
+
 // Simple hello world route
 app.get('/', (req, res) => {
     res.send(`Hello World! Welcome to Pop Pop Chaos Backend. Total clicks: ${clickCount}`);
@@ -26,7 +50,10 @@ io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
 
     // Send current click count to newly connected client
-    socket.emit('clickUpdate', { count: clickCount });
+    socket.emit('clickUpdate', {
+        count: clickCount,
+        reason: 'initial_sync'
+    });
 
     // Handle click events
     socket.on('click', () => {
@@ -34,7 +61,10 @@ io.on('connection', (socket) => {
         console.log(`Click received! Total clicks: ${clickCount}`);
 
         // Broadcast updated count to all connected clients
-        io.emit('clickUpdate', { count: clickCount });
+        io.emit('clickUpdate', {
+            count: clickCount,
+            reason: 'player_click'
+        });
     });
 
     // Handle disconnection
